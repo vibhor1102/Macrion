f066d9fa48a38aacd3dca2a49fdc55aa0ded2e89
vibhor1102 <vibhor1102@gmail.com>
2026-08-31 17:38:36 +0530

    Set Macrion version to 0.1.0

--- smartautoclicker/build.gradle.kts
+++ smartautoclicker/build.gradle.kts
@@ -72,7 +72,7 @@ android {
         applicationId = getExtraActualApplicationId()
 
         versionCode = 1
-        versionName = "0.0.0"
+        versionName = "0.1.0"
     }
 
     if (project.isBuildForVariant(MacrionFlavour.F_DROID, MacrionBuildType.DEBUG)) {
