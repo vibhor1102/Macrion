44b8b367879ab92812a0f0bab29444d76a6335d1
vibhor1102 <vibhor1102@gmail.com>
2026-09-01 12:49:20 +0530

    Prepare Macrion 0.2.0 release

--- smartautoclicker/build.gradle.kts
+++ smartautoclicker/build.gradle.kts
@@ -71,8 +71,8 @@ android {
     defaultConfig {
         applicationId = getExtraActualApplicationId()
 
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     if (project.isBuildForVariant(MacrionFlavour.F_DROID, MacrionBuildType.DEBUG)) {
